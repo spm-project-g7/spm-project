6c778cd0d6bee380119bd65fd9b24abb9eb417ef
imhongkey <chunwang.ng.2019@scis.smu.edu.sg>
2021-11-09 03:24:02 +0800

    Updated on learning-quiz

--- spm website/docker/lesson.py
+++ spm website/docker/lesson.py
@@ -130,7 +130,6 @@ def find_lessonlist_by_class(ClassID):
         }
     ), 404
 
-    # get lesson list by class
 @app.route("/singlelesson/<string:LessonID>")
 def find_singlelesson_by_class(LessonID):
     lessonobj = Lesson.query.filter_by(LessonID=LessonID).first()
