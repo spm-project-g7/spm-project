91410c9f1c3a9a5f4327688b3e5c71e244c88b53
imhongkey <chunwang.ng.2019@scis.smu.edu.sg>
2021-11-09 01:20:58 +0800

    Update on integration

--- spm website/app.py
+++ spm website/app.py
@@ -727,100 +727,118 @@ def find_by_trainerID(TrainerID):
     ), 404
 
 
-# class Material(db.Model):
-#     __tablename__ = 'learningmaterial'
+class Material(db.Model):
+    __tablename__ = 'learningmaterial'
 
-#     MaterialID = db.Column(db.Integer, primary_key=True)
-#     MaterialTitle = db.Column(db.String(255), nullable=False)
-#     MaterialDescription = db.Column(db.String(255), nullable=False)
-#     LessonId = db.Column(db.Integer, nullable=False)
-#     LastUpdated = db.Column(db.Date, nullable=False)
-#     MaterialURL = db.Column(db.String(255), nullable=True)
-#     MaterialType = db.Column(db.String(255), nullable=True)
-#     CompleteStatus = db.Column(db.String(255), nullable=True)
+    MaterialID = db.Column(db.Integer, primary_key=True)
+    MaterialTitle = db.Column(db.String(255), nullable=False)
+    MaterialDescription = db.Column(db.String(255), nullable=False)
+    LessonId = db.Column(db.Integer, nullable=False)
+    LastUpdated = db.Column(db.Date, nullable=False)
+    MaterialURL = db.Column(db.String(255), nullable=True)
+    MaterialType = db.Column(db.String(255), nullable=True)
+    CompleteStatus = db.Column(db.String(255), nullable=True)
 
-#     def __init__(self, MaterialID, MaterialTitle, MaterialDescription, LessonId, LastUpdated, MaterialURL, MaterialType, CompleteStatus):
-#         self.MaterialID = MaterialID
-#         self.MaterialTitle = MaterialTitle
-#         self.MaterialDescription = MaterialDescription
-#         self.LessonId = LessonId
-#         self.LastUpdated = LastUpdated
-#         self.MaterialURL = MaterialURL
-#         self.MaterialType = MaterialType
-#         self.CompleteStatus = CompleteStatus
+    def __init__(self, MaterialID, MaterialTitle, MaterialDescription, LessonId, LastUpdated, MaterialURL, MaterialType, CompleteStatus):
+        self.MaterialID = MaterialID
+        self.MaterialTitle = MaterialTitle
+        self.MaterialDescription = MaterialDescription
+        self.LessonId = LessonId
+        self.LastUpdated = LastUpdated
+        self.MaterialURL = MaterialURL
+        self.MaterialType = MaterialType
+        self.CompleteStatus = CompleteStatus
 
-#     def json(self):
-#         return {"MaterialID": self.MaterialID, "MaterialTitle": self.MaterialTitle, "MaterialDescription": self.MaterialDescription, "LessonId": self.LessonId,
-#                 "LastUpdated": self.LastUpdated, "MaterialURL": self.MaterialURL, "MaterialType": self.MaterialType, "CompleteStatus": self.CompleteStatus}
-
-
-# @app.route("/materials")
-# def get_all_materials():
-#     materiallist = Material.query.all()
-#     if len(materiallist):
-#         return jsonify(
-#             {
-#                 "code": 200,
-#                 "data": {
-#                     "material": [material.json() for material in materiallist]
-#                 }
-#             }
-#         )
-#     return jsonify(
-#         {
-#             "code": 404,
-#             "message": "There are no materials."
-#         }
-#     ), 404
-
-# # get lessonmaterial by lessonID
+    def json(self):
+        return {"MaterialID": self.MaterialID, "MaterialTitle": self.MaterialTitle, "MaterialDescription": self.MaterialDescription, "LessonId": self.LessonId,
+                "LastUpdated": self.LastUpdated, "MaterialURL": self.MaterialURL, "MaterialType": self.MaterialType, "CompleteStatus": self.CompleteStatus}
 
 
-# @app.route("/materials/<string:LessonId>")
-# def find_material_by_lessonID(LessonId):
-#     materiallist = Material.query.filter_by(LessonId=LessonId)
-#     if materiallist:
-#         return jsonify(
-#             {
-#                 "code": 200,
-#                 "data": {
-#                     "material": [material.json() for material in materiallist]
-#                 }
-#             }
-#         ), 200
-#     return jsonify(
-#         {
-#             "code": 404,
-#             "message": "No lesson materials found."
-#         }
-#     ), 404
+@app.route("/materials")
+def get_all_materials():
+    materiallist = Material.query.all()
+    if len(materiallist):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "material": [material.json() for material in materiallist]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no materials."
+        }
+    ), 404
+
+# get lessonmaterial by lessonID
 
 
-# @app.route("/material/<string:LessonID>", methods=['POST'])
-# def create_material(LessonID):
-#     data = request.get_json()
-#     material = Material(LessonID, **data)
+@app.route("/materials/<string:LessonId>")
+def find_material_by_lessonID(LessonId):
+    materiallist = Material.query.filter_by(LessonId=LessonId)
+    if materiallist:
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "material": [material.json() for material in materiallist]
+                }
+            }
+        ), 200
+    return jsonify(
+        {
+            "code": 404,
+            "message": "No lesson materials found."
+        }
+    ), 404
 
-#     try:
-#         db.session.add(material)
-#         db.session.commit()
-#     except:
-#         return jsonify(
-#             {
-#                 "code": 500,
-#                 "data": {
-#                     "user": material.json()
-#                 },
-#                 "message": "An error occurred uploading materials."
-#             }
-#         ), 500
 
-#     return jsonify(
-#         {
-#             "code": 201,
-#             "data": material.json()
-#         }
-#     ), 201
+@app.route("/material/<string:LessonID>", methods=['POST'])
+def create_material(LessonID):
+    data = request.get_json()
+    material = Material(LessonID, **data)
+
+    try:
+        db.session.add(material)
+        db.session.commit()
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "data": {
+                    "user": material.json()
+                },
+                "message": "An error occurred uploading materials."
+            }
+        ), 500
+
+    return jsonify(
+        {
+            "code": 201,
+            "data": material.json()
+        }
+    ), 201
+
+#Get single material by lessonID
+@app.route("/learningmaterial/<string:LessonId>")
+def find_learningmaterial_by_Lesson(LessonId):
+    material = Material.query.filter_by(LessonId=LessonId).first()
+    if material:
+        return jsonify(
+            {
+                "code": 200,
+                "data": material.json()
+            }
+        ), 200
+    return jsonify(
+        {
+            "code": 404,
+            "message": "No material found."
+        }
+    ), 404
 
 
 class Question(db.Model):
@@ -949,71 +967,6 @@ def delete_question(QuizID):
                 "message": "Cannot find question(s) to be deleted."
             },
         ), 404
-
-
-class LearningMaterial(db.Model):
-    __tablename__ = 'learningmaterial'
-
-    MaterialID = db.Column(db.Integer, primary_key=True)
-    MaterialTitle = db.Column(db.String(255), nullable=False)
-    MaterialDescription = db.Column(db.String(255), nullable=False)
-    LessonID = db.Column(db.Integer, nullable=False)
-    LastUpdated = db.Column(db.Date, nullable=False)
-    MaterialURL = db.Column(db.String(255), nullable=True)
-    MaterialType = db.Column(db.String(255), nullable=True)
-    CompleteStatus = db.Column(db.String(255), nullable=True)
-
-    def __init__(self, MaterialID, MaterialTitle, MaterialDescription, LessonID, LastUpdated, MaterialURL, MaterialType, CompleteStatus):
-        self.MaterialID = MaterialID
-        self.MaterialTitle = MaterialTitle
-        self.MaterialDescription = MaterialDescription
-        self.LessonID = LessonID
-        self.LastUpdated = LastUpdated
-        self.MaterialURL = MaterialURL
-        self.MaterialType = MaterialType
-        self.CompleteStatus = CompleteStatus
-
-    def json(self):
-        return {"MaterialID": self.MaterialID, "MaterialTitle": self.MaterialTitle, "MaterialDescription": self.MaterialDescription, "LessonId": self.LessonID,
-                "LastUpdated": self.LastUpdated, "MaterialURL": self.MaterialURL, "MaterialType": self.MaterialType, "CompleteStatus": self.CompleteStatus}
-
-
-@app.route("/learningmaterial")
-def get_all_material():
-    materiallist = LearningMaterial.query.all()
-    if len(materiallist):
-        return jsonify(
-            {
-                "code": 200,
-                "data": {
-                    "materials": [material.json() for material in materiallist]
-                }
-            }
-        )
-    return jsonify(
-        {
-            "code": 404,
-            "message": "No material found."
-        }
-    ), 404
-
-
-@app.route("/learningmaterial/<string:LessonID>")
-def find_learningmaterial_by_Lesson(LessonID):
-    material = LearningMaterial.query.filter_by(LessonID=LessonID).first()
-    if material:
-        return jsonify(
-            {
-                "code": 200,
-                "data": material.json()
-            }
-        ), 200
-    return jsonify(
-        {
-            "code": 404,
-            "message": "No classes found."
-        }
-    ), 404
 
 
 if __name__ == '__main__':
